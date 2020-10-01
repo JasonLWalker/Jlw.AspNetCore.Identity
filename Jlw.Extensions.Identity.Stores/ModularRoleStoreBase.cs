0e8cf72c719db1d657a3e5a882618c1dcfb77bda
Jason Walker <jlwalker@spsmail.org>
2020-10-01 08:02:08 -0500

    Add Role Store

---
+++ Jlw.Extensions.Identity.Stores/ModularRoleStoreBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Jlw.Extensions.Identity.Stores
+{
+    public class ModularRoleStoreBase : ModularRoleStoreBase<IModularBaseRole<int>>
+    {
+
+    }
+
+    public class ModularRoleStoreBase<TKey> : IRoleStore<IModularBaseRole<TKey>>
+    {
+        public virtual void Dispose()
+        {
+            // Nothing to dispose
+        }
+
+        public virtual Task<IdentityResult> CreateAsync(IModularBaseRole<TKey> role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<IdentityResult> UpdateAsync(IModularBaseRole<TKey> role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<IdentityResult> DeleteAsync(IModularBaseRole<TKey> role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<string> GetRoleIdAsync(IModularBaseRole<TKey> role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<string> GetRoleNameAsync(IModularBaseRole<TKey> role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task SetRoleNameAsync(IModularBaseRole<TKey> role, string roleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<string> GetNormalizedRoleNameAsync(IModularBaseRole<TKey> role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task SetNormalizedRoleNameAsync(IModularBaseRole<TKey> role, string normalizedName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<IModularBaseRole<TKey>> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<IModularBaseRole<TKey>> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
