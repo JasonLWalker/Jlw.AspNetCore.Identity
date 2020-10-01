9db121ca4c6eff91535ca22eb13fd7518cf86914
Jason Walker <jlwalker@spsmail.org>
2020-10-01 08:01:05 -0500

    Added Modular interfaces

---
+++ Jlw.Extensions.Identity.Stores/Interfaces/IModularBaseRole.cs
@@ -0,0 +1,11 @@
+﻿namespace Jlw.Extensions.Identity.Stores
+{
+    public interface IModularBaseRole : IModularBaseRole<int> { }
+
+    public interface IModularBaseRole<TKey>
+    {
+        TKey Id { get; set; }
+        string Name { get; set; }
+        string NormalizedName { get; set; }
+    }
+}
---
+++ Jlw.Extensions.Identity.Stores/Interfaces/IModularBaseUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace Jlw.Extensions.Identity.Stores
+{
+    public interface IModularBaseUser : IModularBaseUser<string>
+    {
+
+    }
+
+    public interface IModularBaseUser<TKey> : IModularBaseUser<TKey, IdentityUserClaim<TKey>>
+        where TKey : IEquatable<TKey>
+    {
+    }
+
+    public interface IModularBaseUser<TKey, TClaim>
+        where TKey : IEquatable<TKey>
+    {
+        TKey Id { get; set; }
+        string UserName { get; set; }
+        string NormalizedUserName { get; set; }
+
+        string Email { get; set; }
+        string NormalizedEmail { get; set; }
+        bool EmailConfirmed { get; set; }
+
+
+        string PasswordHash { get; set; }
+
+        string PhoneNumber { get; set; }
+        bool PhoneNumberConfirmed { get; set; }
+
+        bool TwoFactorEnabled { get; set; }
+
+        int AccessFailedCount { get; set; }
+
+        bool LockoutEnabled { get; set; }
+
+        DateTimeOffset? LockoutEnd { get; set; }
+
+        ICollection<TClaim> Claims { get; }
+
+        IModularBaseUser<TKey, TClaim> CopyFrom(IModularBaseUser<TKey, TClaim> o);
+
+        IModularBaseUser<TKey, TClaim> CopyFrom(object o);
+    }
+}
