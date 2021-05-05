d1d398db4e4d3a46a2c250d2532656a9606b6178
Jason Walker <jason@draxjinn.com>
2021-05-05 12:24:17 -0500

    Added razor overrides without Entity Framework

---
+++ TestMvcWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+//[assembly: HostingStartup(typeof(TestMvcWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace TestMvcWebApp.Areas.Identity
+{
+    /*
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                / *
+
+                        services.AddDbContext<TestMvcWebAppContext>(options =>
+                            options.UseSqlite(
+                                context.Configuration.GetConnectionString("TestMvcWebAppContextConnection")));
+
+                            .AddEntityFrameworkStores<TestMvcWebAppContext>();
+                    * /
+                services.AddDefaultIdentity<Jlw.Extensions.Identity.Stores.ModularBaseUser<long>>(options => options.SignIn.RequireConfirmedAccount = true);
+            });
+        }
+    }
+    */
+}
