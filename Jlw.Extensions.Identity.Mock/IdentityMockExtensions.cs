11344ec775bbfbeb599e74551491529223ff97c5
Jason Walker <jason@draxjinn.com>
2021-05-05 12:23:35 -0500

    Added Mock Identity

---
+++ Jlw.Extensions.Identity.Mock/IdentityMockExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Jlw.Extensions.Identity.Stores;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Jlw.Extensions.Identity.Mock;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class IdentityMockExtensions
+    {
+        public static IServiceCollection AddIdentityMocking<TUser>(this IServiceCollection services) where TUser : ModularBaseUser, new()
+        {
+            services.AddTransient<IUserStore<TUser>>(MockUserStore<TUser>.UserStoreFactory);
+            services.AddTransient<IRoleStore<IModularBaseRole<long>>, ModularRoleStoreBase<long>>();
+            services.AddTransient<IPasswordHasher<TUser>>(sp => new PlainTextPasswordHasher<TUser>());
+
+            services.AddDefaultIdentity<TUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;
+                    options.User.RequireUniqueEmail = true;
+                })
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
+
+            services.AddRazorPages(options => { }); // Adds Identity UI and other Razor pages
+
+            services.PostConfigure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme, option =>
+            {
+                //option.Cookie.Name = $".{config.SiteId}.Identity.{config.LoginType}"; // change cookie name
+                //option.Cookie.Path = (config.WebRootPath ?? "/").TrimEnd('/');
+                option.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+                option.SlidingExpiration = true;
+            });
+
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityMocking(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
---
+++ Jlw.Extensions.Identity.Mock/MockUserStore.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Jlw.Extensions.Identity.Stores;
+
+namespace Jlw.Extensions.Identity.Mock
+{
+    public class MockUserStore<TUser> : ModularUserStoreBase<TUser> where TUser : ModularBaseUser, new()
+    {
+        protected static List<TUser> MockedUsers = new List<TUser>();
+
+        public static IUserStore<TUser> UserStoreFactory(IServiceProvider arg)
+        {
+            return new MockUserStore<TUser>();
+        }
+        
+        public override Task<TUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(MockedUsers.FirstOrDefault(o=>o.NormalizedUserName.Equals(normalizedUserName, StringComparison.InvariantCultureIgnoreCase)));
+        }
+
+        public override Task<TUser> FindByIdAsync(string id, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(MockedUsers.FirstOrDefault(o => o.Id.Equals(id, StringComparison.InvariantCultureIgnoreCase)));
+        }
+
+        public static void AddMockedUser(TUser user, IEnumerable<IdentityUserClaim<string>> claims = null)
+        {
+            TUser u = (TUser)user.CopyFrom(user);
+            if (claims != null)
+            {
+                foreach (var claim in claims)
+                {
+                    u.Claims.Add(claim);
+                }
+            }
+
+            MockedUsers.Add(u);
+        }
+    }
+}
