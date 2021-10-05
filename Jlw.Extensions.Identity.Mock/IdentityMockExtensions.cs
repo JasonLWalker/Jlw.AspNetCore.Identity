29b50f210dbdebb120b7501e1ab18ed7ced5778b
Jason Walker <jason@draxjinn.com>
2021-10-05 16:12:51 -0500

    Add ability to mock with key

--- Jlw.Extensions.Identity.Mock/IdentityMockExtensions.cs
+++ Jlw.Extensions.Identity.Mock/IdentityMockExtensions.cs
@@ -36,6 +36,36 @@ namespace Microsoft.Extensions.DependencyInjection
 
             return services;
         }
+        public static IServiceCollection AddIdentityMocking<TUser, TKey>(this IServiceCollection services)
+            where TKey : IEquatable<TKey> 
+            where TUser : ModularBaseUser<TKey>, IModularBaseUser<TKey>, new()
+            
+        {
+            services.AddTransient<IUserStore<TUser>>(MockUserStore<TUser, TKey>.UserStoreFactory);
+            services.AddTransient<IRoleStore<IModularBaseRole<TKey>>, ModularRoleStoreBase<TKey>>();
+            services.AddTransient<IPasswordHasher<TUser>>(sp => new PlainTextPasswordHasher<TUser>());
+
+            services.AddDefaultIdentity<TUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;
+                    options.User.RequireUniqueEmail = true;
+                })
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
+
+            services.AddRazorPages(options => { }); // Adds Identity UI and other Razor pages
+
+            services.PostConfigure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme, option =>
+            {
+                //option.Cookie.Name = $".{config.SiteId}.Identity.{config.LoginType}"; // change cookie name
+                //option.Cookie.Path = (config.WebRootPath ?? "/").TrimEnd('/');
+                option.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+                option.SlidingExpiration = true;
+            });
+
+
+            return services;
+        }
 
         public static IApplicationBuilder UseIdentityMocking(this IApplicationBuilder app)
         {
--- Jlw.Extensions.Identity.Mock/MockUserStore.cs
+++ Jlw.Extensions.Identity.Mock/MockUserStore.cs
@@ -8,13 +8,21 @@ using Jlw.Extensions.Identity.Stores;
 
 namespace Jlw.Extensions.Identity.Mock
 {
-    public class MockUserStore<TUser> : ModularUserStoreBase<TUser> where TUser : ModularBaseUser, new()
+    public class MockUserStore<TUser> : MockUserStore<TUser, string> where TUser : ModularBaseUser, new()
+    {
+
+    }
+
+
+    public class MockUserStore<TUser, TKey> : ModularUserStoreBase<TUser, TKey> 
+        where TUser : ModularBaseUser<TKey>, new()
+        where TKey : IEquatable<TKey>
     {
         protected static List<TUser> MockedUsers = new List<TUser>();
 
         public static IUserStore<TUser> UserStoreFactory(IServiceProvider arg)
         {
-            return new MockUserStore<TUser>();
+            return new MockUserStore<TUser, TKey>();
         }
         
         public override Task<TUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
@@ -24,10 +32,10 @@ namespace Jlw.Extensions.Identity.Mock
 
         public override Task<TUser> FindByIdAsync(string id, CancellationToken cancellationToken)
         {
-            return Task.FromResult(MockedUsers.FirstOrDefault(o => o.Id.Equals(id, StringComparison.InvariantCultureIgnoreCase)));
+            return Task.FromResult(MockedUsers.FirstOrDefault(o =>  o?.Id?.ToString()?.Equals(id, StringComparison.InvariantCultureIgnoreCase) ?? default));
         }
 
-        public static void AddMockedUser(TUser user, IEnumerable<IdentityUserClaim<string>> claims = null)
+        public static void AddMockedUser(TUser user, IEnumerable<IdentityUserClaim<TKey>> claims = null)
         {
             TUser u = (TUser)user.CopyFrom(user);
             if (claims != null)
