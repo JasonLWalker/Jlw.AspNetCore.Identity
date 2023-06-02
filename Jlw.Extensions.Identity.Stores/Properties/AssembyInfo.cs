5d66c0bb54d0129e0283c11e7fba1cadcfcda323
Jason Walker <jason@draxjinn.com>
2023-06-02 11:50:58 -0500

    update project for new build workflow

--- Jlw.Extensions.Identity.Stores/Properties/AssembyInfo.cs
+++ Jlw.Extensions.Identity.Stores/Properties/AssembyInfo.cs
@@ -1,3 +1,4 @@
+/*
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("3.0.*")]
+*/
