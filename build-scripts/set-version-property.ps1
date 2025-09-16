328f24a98c52ab66fdc8a7bdcae4d6de73b6b491
Jason Walker <jason@draxjinn.com>
2025-09-16 12:57:10 -0500

    bump version to 4.0 due to namespace changes

--- build-scripts/set-version-property.ps1
+++ build-scripts/set-version-property.ps1
@@ -1,9 +1,12 @@
 param([string]$buildType="Release", [string]$versionSuffix="", [string]$versionPrefix="")
 
 if (-Not ($versionPrefix)){
-	$versionPrefix="3.0.$([System.TimeSpan]::FromTicks($([System.DateTime]::UtcNow.Ticks)).Subtract($([System.TimeSpan]::FromTicks(630822816000000000))).TotalDays.ToString().SubString(0,9))"
+	$versionPrefix="4.0.$([System.TimeSpan]::FromTicks($([System.DateTime]::UtcNow.Ticks)).Subtract($([System.TimeSpan]::FromTicks(630822816000000000))).TotalDays.ToString().SubString(0,9))"
 }
 
+$versionSuffix = "$versionSuffix+$([System.DateTime]::UtcNow.ToString("yyyyMMdd-HHmmss"))"
 $global:vPrefix = $versionPrefix
 $global:vSuffix = $versionSuffix
+$global:versionPrefix = $versionPrefix
+
 Write-Host $versionPrefix$versionSuffix
