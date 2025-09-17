ca2cd59b4a548bc6cfce342f3c6ff6826edc6c3e
Jason Walker <jason@draxjinn.com>
2025-09-17 14:52:05 -0500

    Add Base interface

--- Jlw.Extensions.Identity.Abstractions/Models/ModularBaseRole.cs
+++ Jlw.Extensions.Identity.Abstractions/Models/ModularBaseRole.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Jlw.Extensions.Identity
 {
-    public class ModularBaseRole : ModularBaseRole<int> { }
+    public class ModularBaseRole : ModularBaseRole<int>, IModularBaseRole { }
 
 
     public class ModularBaseRole<TKey> : IModularBaseRole<TKey>
