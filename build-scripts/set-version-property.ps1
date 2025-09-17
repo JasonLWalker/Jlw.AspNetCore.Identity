49c4ae6190348f15616cedb674bb513ac10b6160
Jason Walker <jason@draxjinn.com>
2025-09-17 15:12:37 -0500

    fix broken version suffix

--- build-scripts/set-version-property.ps1
+++ build-scripts/set-version-property.ps1
@@ -4,7 +4,7 @@ if (-Not ($versionPrefix)){
 	$versionPrefix="4.0.$([System.TimeSpan]::FromTicks($([System.DateTime]::UtcNow.Ticks)).Subtract($([System.TimeSpan]::FromTicks(630822816000000000))).TotalDays.ToString().SubString(0,9))"
 }
 
-$versionSuffix = "$versionSuffix+$([System.DateTime]::UtcNow.ToString("yyyyMMdd-HHmmss"))"
+#$versionSuffix = "$versionSuffix+$([System.DateTime]::UtcNow.ToString("yyyyMMdd-HHmmss"))"
 $global:vPrefix = $versionPrefix
 $global:vSuffix = $versionSuffix
 $global:versionPrefix = $versionPrefix
